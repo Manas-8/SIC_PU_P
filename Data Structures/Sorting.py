a5292fbe9b8def1a78c28aa2d6621a7978d56e35
Manas-8 <manasdiwan8@gmail.com>
2025-07-02 14:51:27 +0530

    Update Sorting.py

--- Data Structures/Sorting.py
+++ Data Structures/Sorting.py
@@ -29,3 +29,30 @@ def selection_sort(arr):
 
 my_list = [64, 34, 25, 12, 22, 11, 90, 5]
 print(selection_sort(my_list))
+
+
+# Quick Sort
+def partition(arr, low, high):
+    pivot = arr[high]
+    k = low - 1
+    
+    for j in range(low, high):
+        if arr[j] < pivot:
+            k+=1
+            arr[k], arr[j] = arr[j], arr[k]
+    arr[k+1], arr[high] = arr[high], arr[k+1]
+    return k+1
+        
+def quick_sort(arr, low=0, high=None):
+    if high == None:
+        high = len(arr) - 1
+        
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr, pivot_index + 1, high)
+        
+    
+my_list=[34,67,45,23,5,65,12,39,28]
+quick_sort(my_list)
+print(my_list)
