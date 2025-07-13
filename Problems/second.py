597ffb58400568fe30a07a7ad1b5d02b751b721f
Manas-8 <manasdiwan8@gmail.com>
2025-07-13 20:31:33 +0530

    Update second.py

--- Problems/second.py
+++ Problems/second.py
@@ -9,3 +9,32 @@ for i in range(number_of_oranges):
 orange_sizes[k], orange_sizes[number_of_oranges - 1], orange_sizes[number_of_oranges - 1], orange_sizes[k]
 
 print(orange_sizes)
+
+
+#Harry Potter problem
+def harry_potter_stone():
+    n = int(input())
+    coins = list(map(int, input().split()))
+    q, x = map(int, input().split())
+    instructions = [input().strip() for _ in range(q)]
+
+    stack = []
+    index = 0
+    total = 0
+
+    for ins in instructions:
+        if ins == "Harry":
+            if index < n:
+                coin = coins[index]
+                stack.append(coin)
+                total += coin
+                index += 1
+        elif ins == "Remove":
+            if stack:
+                total -= stack.pop()
+
+        if total == x:
+            print(len(stack))
+            return
+
+    print(-1)
